26e3b3dad70a1cbcd8352fcc34ca080933a3b2e1
henrikviking 1 <henrials@stud.ntnu.no>
2025-01-12 12:57:37 +0100

    Updated Go code

--- shared variable/c/foo.c
+++ shared variable/c/foo.c
@@ -7,12 +7,16 @@
 
 int i = 0;
 
+pthread_mutex_t mutex; // Declaring mutex globally
+
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
     
     for (int j = 0; j < 1000000; j++) {
+        pthread_mutex_lock(&mutex);
         i++;
+        pthread_mutex_unlock(&mutex);
     }
     
     return NULL;
@@ -21,8 +25,10 @@ void* incrementingThreadFunction(){
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
     
-    for (int k = 0; k < 1000000; k++) {
+    for (int k = 0; k < 999999; k++) {
+        pthread_mutex_lock(&mutex);
         i--;
+        pthread_mutex_unlock(&mutex);
     }
 
     return NULL;
@@ -36,12 +42,18 @@ int main(){
 
     pthread_t thread1, thread2; // Declaring threads
 
+    pthread_mutex_init(&mutex, NULL); //Initializing the mutex
+
+    // Here I am implementing MUTEX since it is a binary semaphore and because the counting semaphore is more effective to inform several threads that a resource is available.
+
     pthread_create(&thread1, NULL, incrementingThreadFunction, NULL); // Creating the incrementing thread
     pthread_create(&thread2, NULL, decrementingThreadFunction, NULL); // Creating the decrementing thread
     
     // TODO:
     // wait for the two threads to be done before printing the final result
     // Hint: Use `pthread_join`   
+
+    pthread_mutex_destroy(&mutex); // Destroying the mutex
 
     pthread_join(thread1, NULL); // Waiting for the incrementing thread to finish
     pthread_join(thread2, NULL); // Waiting for the decrementing thread to finish
